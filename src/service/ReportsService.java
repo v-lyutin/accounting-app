02823074bb5b9ce96b8e5fb21ca3177c9f64b6d2
Vladislav <wladushek21@gmail.com>
2023-06-27 01:50:07 +0300

    style: осуществить переносы строк, превышающих 120 символов

--- src/service/ReportsService.java
+++ src/service/ReportsService.java
@@ -15,7 +15,9 @@ public class ReportsService {
 
     public void checkReports() {
         if (yearlyReportManager.yearlyTransactions.isEmpty() && monthlyReportManager.monthlyReports.isEmpty()) {
-            System.out.println(InputHandler.ANSI_RED + "Отчеты пока не сформированы. Воспользуйтесь для начала командой [1] и [2]\n" + InputHandler.ANSI_RED);
+            System.out.println(InputHandler.ANSI_RED
+                    + "Отчеты пока не сформированы. Воспользуйтесь для начала командой [1] и [2]\n"
+                    + InputHandler.ANSI_RED);
             return;
         } else {
 
@@ -27,13 +29,15 @@ public class ReportsService {
                     if (monthlyExpenses.get(yearlyTransaction.month) == yearlyTransaction.amount)
                         printMatch(yearlyTransaction.month, true);
                     else {
-                        printMismatch(yearlyTransaction.month, monthlyExpenses.get(yearlyTransaction.month), yearlyTransaction.amount, true);
+                        printMismatch(yearlyTransaction.month, monthlyExpenses.get(yearlyTransaction.month),
+                                yearlyTransaction.amount, true);
                     }
                 } else {
                     if (monthlyIncome.get(yearlyTransaction.month) == yearlyTransaction.amount)
                         printMatch(yearlyTransaction.month, false);
                     else {
-                        printMismatch(yearlyTransaction.month, monthlyExpenses.get(yearlyTransaction.month), yearlyTransaction.amount, false);
+                        printMismatch(yearlyTransaction.month, monthlyExpenses.get(yearlyTransaction.month),
+                                yearlyTransaction.amount, false);
                     }
                 }
             }
@@ -43,18 +47,24 @@ public class ReportsService {
 
     private void printMismatch(String month, double monthlyAmount, double yearlyAmount, boolean isExpense) {
         if (isExpense) {
-            System.out.println(InputHandler.ANSI_RED + "[!] Обнаружено несоответствие расходов за " + month + InputHandler.ANSI_RED);
-            System.out.println("В месячном отчете расходы составляют - " + monthlyAmount + ", в годовом - " + yearlyAmount);
+            System.out.println(InputHandler.ANSI_RED + "[!] Обнаружено несоответствие расходов за " + month
+                    + InputHandler.ANSI_RED);
+            System.out.println("В месячном отчете расходы составляют - " + monthlyAmount + ", в годовом - "
+                    + yearlyAmount);
         } else {
-            System.out.println(InputHandler.ANSI_RED + "[!] Обнаружено несоответствие доходов за " + month + InputHandler.ANSI_RED);
-            System.out.println("В месячном отчете доходы составляют - " + monthlyAmount + ", в годовом - " + yearlyAmount);
+            System.out.println(InputHandler.ANSI_RED + "[!] Обнаружено несоответствие доходов за " + month
+                    + InputHandler.ANSI_RED);
+            System.out.println("В месячном отчете доходы составляют - " + monthlyAmount + ", в годовом - "
+                    + yearlyAmount);
         }
     }
 
     private void printMatch(String month, boolean isExpense) {
         if (isExpense)
-            System.out.println(InputHandler.ANSI_WHITE + "[✔] Сверка расходов за " + month + " прошла успешно" + InputHandler.ANSI_WHITE);
+            System.out.println(InputHandler.ANSI_WHITE + "[✔] Сверка расходов за " + month + " прошла успешно"
+                    + InputHandler.ANSI_WHITE);
         else
-            System.out.println(InputHandler.ANSI_WHITE + "[✔] Сверка доходов за " + month + " прошла успешно" + InputHandler.ANSI_WHITE);
+            System.out.println(InputHandler.ANSI_WHITE + "[✔] Сверка доходов за " + month + " прошла успешно"
+                    + InputHandler.ANSI_WHITE);
     }
 }
